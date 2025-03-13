942353dc6da06be4d509090d544e35252d7602ca
Rabiya46 <rabiyaaiylchieva@gmail.com>
2025-03-13 15:18:20 +0600

    add functions

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,5 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { incrementByCount } from "../../store/counter/counterSlice";
+import { useEffect } from "react";
+import { COUNTER } from "../../store/counter/counterThunk";
+
 const Button = () => {
-  return <div>Button</div>;
+  const { value, users } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => dispatch(incrementByCount(5));
+
+  useEffect(() => {
+    dispatch(COUNTER.getAllUsers());
+  }, []);
+
+  console.log(users);
+
+  return (
+    <div>
+      <h1>Counter: {value}</h1>
+
+      <button onClick={handleIncrement}>increment</button>
+      <button>decrement</button>
+    </div>
+  );
 };
 
 export default Button;
